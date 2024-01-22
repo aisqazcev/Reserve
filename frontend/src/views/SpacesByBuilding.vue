c238f4b1a0108b4b0738586a5585b204887f6995
olisanrod <olivasanchez14@hotmail.com>
2024-01-22 18:02:34 +0100

    Añadida información del building.

--- frontend/src/views/SpacesByBuilding.vue
+++ frontend/src/views/SpacesByBuilding.vue
@@ -12,6 +12,7 @@
           <span></span>
           <span></span>
           <span></span>
+
           <span></span>
         </div>
         <div class="container shape-container d-flex">
@@ -19,14 +20,23 @@
           <div class="col px-0">
             <div class="row">
               <div class="col-lg-6">
-                <h1 class="display-3  text-white mb-3">{{ building.name }}</h1>
+                <h1 class="display-3  text-white mb-2">{{ building.name_complete }}</h1>
                 <div class="d-flex align-items-center mb-3">
-                <i class="ni ni-pin-3  mr-2" style="font-size: 24px; color:white;"></i>
-                <p class="text-white">{{ building.address }}</p>
+                <i class="ni ni-square-pin mr-2" style="font-size: 24px; color:white;"></i>
+                <p class="text-white mb-0">{{ building.address }}</p>
                 </div>
-                <p class="text-white">Web: {{ building.web }}</p>
-                <p class="text-white">Email: {{ building.email }}</p>
-                <p class="text-white">Telefono: {{ building.phone }}</p>
+                <div class="d-flex align-items-center mb-3">
+                <i class="ni ni-world-2 mr-2" style="font-size: 24px; color:white;"></i>
+                <p class="text-white mb-0">{{ building.web }}</p>
+                </div>
+                <div class="d-flex align-items-center mb-3">
+                <i class="ni ni-email-83 mr-2" style="font-size: 24px; color:white;"></i>
+                <p class="text-white mb-0">{{ building.email }}</p>
+                </div>
+                <div class="d-flex align-items-center mb-3">
+                <i class="ni ni-mobile-button mr-2" style="font-size: 24px; color:white;"></i>
+                <p class="text-white mb-0">{{ building.phone }}</p>
+                </div>
                 <p class="text-white">Servicios: {{ building.services }}</p>
               </div>
             </div>
