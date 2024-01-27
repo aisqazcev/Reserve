4cfce400f2ad7a85adbc3c42057dc57dda56d481
olisanrod <olivasanchez14@hotmail.com>
2024-01-27 20:52:36 +0100

    Cambios en la config del settings.py.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -151,7 +151,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-        'rest_framework.authentication.TokenAuthentication',
         
     ),
      'DEFAULT_AUTHENTICATION_CLASSES': (
