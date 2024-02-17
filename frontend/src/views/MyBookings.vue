7cc484e7d26e18b14bc444ec610ea1c69acf5fb7
Aisha <aisqazcev@alum.us.es>
2024-02-17 17:54:35 +0100

    Actualizar vistas MyBookings y SpaceList

--- frontend/src/views/MyBookings.vue
+++ frontend/src/views/MyBookings.vue
@@ -6,29 +6,42 @@
       <span></span>
       <span style="background-color: #787CFF;"></span>
     </div>
+
     <div class="container mt-3">
       <div class="table-container">
         <table class="table">
           <thead>
             <tr>
-              <th>id</th>
-              <th>Fecha</th>
-              <th>Inicio</th>
-              <th>Duración</th>
-              <th>Campus</th>
-              <th>Facultad</th>
-              <th class="text-right">Acciones</th>
+              <th>
+                Fecha
+                <i
+                  class="fas fa-sort ml-2"
+                  :class="
+                    sortDirection === 'asc' ? 'fa-sort-up' : 'fa-sort-down'
+                  "
+                  @click="toggleSortDirection"
+                ></i>
+              </th>
+
+              <th>Hora inicio</th>
+              <th>Hora fin</th>
+              <th>Asiento</th>
+              <th>Sala</th>
+              <th>Edificio</th>
+              <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(item, index) in displayedBookings" :key="index">
-              <td>{{ item.id }}</td>
-              <td>{{ item.date }}</td>
-              <td>{{ item.start_time }}</td>
-              <td>{{ item.end_time }}</td>
-              <td>{{ item.campus }}</td>
-              <td>{{ item.building }}</td>
-              <td class="td-actions text-right">
+              <td data-label="Fecha">{{ item.date }}</td>
+              <td data-label="Hora inicio">
+                {{ formatHour(item.start_time) }}
+              </td>
+              <td data-label="Hora fin">{{ formatHour(item.end_time) }}</td>
+              <td data-label="Asiento">{{ item.deskName }}</td>
+              <td data-label="Sala">{{ item.spaceName }}</td>
+              <td data-label="Edificio">{{ item.buildingName }}</td>
+              <td class="td-actions">
                 <button
                   type="button"
                   rel="tooltip"
@@ -89,50 +102,108 @@ export default {
       currentPage: 1,
       perPage: 5,
       totalItems: 0,
+      isLoading: true,
+      sortDirection: "desc",
     };
   },
   mounted() {
     this.listBookings();
+    this.sortBookingsByDate();
   },
   computed: {
     displayedBookings() {
-      if (this.booking) {
-        const startIndex = (this.currentPage - 1) * this.perPage;
-        const endIndex = startIndex + this.perPage;
-        return this.booking.slice(startIndex, endIndex);
-      } else {
-        return [];
-      }
+      if (!this.booking) return [];
+      const startIndex = (this.currentPage - 1) * this.perPage;
+      const endIndex = startIndex + this.perPage;
+      return this.booking.slice(startIndex, endIndex);
     },
   },
   methods: {
-    listBookings() {
-      const token = localStorage.getItem("token");
-      if (token) {
-        axios
-          .get(`${backendUrl}bookings/`, {
-            headers: { Authorization: `Token ${token}` },
-          })
-          .then((response) => {
-            this.booking = response.data;
-            this.totalItems = response.data.length;
-         
-          })
-          .catch((error) => {
-            console.error("Error en la obtención de reservas:", error);
-          });
-      } else {
-        console.error("No se encontró el token de autenticación.");
+    toggleSortDirection() {
+      this.sortDirection = this.sortDirection === "asc" ? "desc" : "asc";
+      this.sortBookingsByDate();
+    },
+    sortBookingsByDate() {
+      if (this.booking) {
+        this.booking.sort((a, b) => {
+          const dateA = new Date(a.date);
+          const dateB = new Date(b.date);
+
+          const directionFactor = this.sortDirection === "asc" ? 1 : -1; 
+          
+          if (dateA > dateB) return -1 * directionFactor;
+          if (dateA < dateB) return 1 * directionFactor;
+
+          const timeA = new Date(a.start_time);
+          const timeB = new Date(b.start_time);
+          return (timeA - timeB) * directionFactor;
+        });
       }
+    },
+    async listBookings() {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("No se encontró el token de autenticación.");
+        return;
+      }
+      try {
+        const response = await axios.get(`${backendUrl}bookings/`, {
+          headers: { Authorization: `Token ${token}` },
+        });
+        this.booking = response.data;
+        this.totalItems = this.booking.length;
+        await this.getBookingDetails();
+      } catch (error) {
+        console.error("Error en la obtención de reservas:", error);
+      }
+    },
+    getBookingDetails() {
+      const deskPromises = [];
+      const spacePromises = [];
+      const buildingPromises = [];
+
+      this.booking.forEach((booking) => {
+        deskPromises.push(axios.get(`${backendUrl}desk/${booking.desk}/`));
+        spacePromises.push(axios.get(`${backendUrl}spaces/${booking.space}/`));
+        buildingPromises.push(
+          axios.get(`${backendUrl}building/${booking.building}/`)
+        );
+      });
+
+      Promise.all([...deskPromises, ...spacePromises, ...buildingPromises])
+        .then((responses) => {
+          this.booking.forEach((booking, index) => {
+            const deskIndex = index;
+            const spaceIndex = index + this.booking.length;
+            const buildingIndex = index + this.booking.length * 2;
+            booking.deskName = responses[deskIndex].data.name;
+            booking.spaceName = responses[spaceIndex].data.name;
+            booking.buildingName = responses[buildingIndex].data.name;
+          });
+
+          this.isLoading = false;
+
+          this.$forceUpdate();
+        })
+        .catch((error) => {
+          console.error("Error obteniendo detalles:", error);
+          this.isLoading = false;
+        });
     },
     onPageChange(page) {
       this.currentPage = page;
     },
+    formatHour(dateTime) {
+      const date = new Date(dateTime);
+      return date.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+    },
   },
 };
 </script>
-
-
 
 <style>
 .table-container {
@@ -142,5 +213,34 @@ export default {
 }
 .table-container table {
   background-color: transparent;
+  width: 100%;
+}
+.table td {
+  text-align: center;
+}
+.table thead th {
+  text-align: center;
+}
+.table tbody tr:nth-child(odd) {
+  background-color: #f2f2f2;
+}
+
+@media only screen and (max-width: 600px) {
+  .table thead {
+    display: none;
+  }
+  .table td {
+    display: block;
+    width: 100%;
+    text-align: left;
+    padding: 10px;
+  }
+  .table td::before {
+    content: attr(data-label);
+    font-weight: bold;
+    float: left;
+    text-transform: capitalize;
+    padding-right: 5px;
+  }
 }
 </style>
